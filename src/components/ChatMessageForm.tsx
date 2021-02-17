7ca67faf6a98aa4e2f786e68af91d22242defd57
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-17 19:11:14 +0900

    送信時にフォームをリセットする

--- src/components/ChatMessageForm.tsx
+++ src/components/ChatMessageForm.tsx
@@ -1,15 +1,12 @@
 import React, {useCallback} from "react";
 import {useForm} from "react-hook-form";
 import {ChatMessageApi} from "../client-axios";
-import {TextField} from "@material-ui/core";
 
 type Props = {}
 
 type Inputs = {
     chatMessage: string,
 };
-
-type Reset = (values?: Record<string, any>) => void;
 
 const ChatMessageForm: React.FC<Props> = ({}) => {
     const api = new ChatMessageApi();
@@ -18,17 +15,13 @@ const ChatMessageForm: React.FC<Props> = ({}) => {
     const onSubmit = (data: Inputs) => {
         console.log(data);
         const msg = {eventAbbr: "cndo2021", roomId: 1, roomType: "talk", body: data.chatMessage}
-        console.log(msg)
-        reset()
         api.apiV1ChatMessagesPost(msg)
     };
-    const onReset = useCallback(() => reset(), [reset])
+    const onReset = useCallback(() => reset({ chatMessage: "" }), [reset])
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <label>
-                <input type="text" name="chatMessage" ref={register()} />
-            </label>
+            <input name="chatMessage" ref={register} />
             <input type="submit" onClick={onReset}/>
         </form>
     )
