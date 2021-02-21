e29c73c34d1cfff5ddb7133f5042d8a5a671c6bc
MakTak <justaway22@gmail.com>
2021-02-21 21:24:25 +0900

    PlayerにStyled-Componentsを導入

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,35 +1,25 @@
-import * as React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react'
+import * as Styled from './styled'
 
 type Props = {
   vimeoId?: string
   autoplay: boolean
 }
 
-const useStyles = makeStyles({
-  player: {
-    width: '100%',
-    height: '500px',
-    backgroundColor: '#123',
-  },
-})
-
 export const Player: React.FC<Props> = ({ vimeoId, autoplay }) => {
-  const classes = useStyles()
   return (
-    <iframe
+    <Styled.Container
       src={
         'https://player.vimeo.com/video/' +
         vimeoId +
         '?autoplay=' +
         Number(autoplay)
       }
-      className={classes.player}
       frameBorder="0"
       allow="autoplay; fullscreen"
       //webkitallowfullscreen
       //mozallowfullscreen
       allowFullScreen
-    ></iframe>
+    ></Styled.Container>
   )
 }
