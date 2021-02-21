e5278b6a2fc90858f9f6c7be40b93e3579a2b2f3
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-21 17:34:57 +0900

    リアクション用ボタンを設置

--- src/components/Chat/ChatMessageForm.tsx
+++ src/components/Chat/ChatMessageForm.tsx
@@ -1,55 +1,79 @@
 import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { ChatMessageApi, ChatMessageMessageTypeEnum } from '../../client-axios'
+import Button from '@material-ui/core/Button'
 
 type Props = {
-    roomId?: number
+  roomId?: number
 }
 
 type Inputs = {
-    chatMessage: string
-    isQuestion: boolean
+  chatMessage: string
+  isQuestion: boolean
+}
+
+const ChatMessageRequest = (eventAbbr: string, roomId: number, roomType: string, body: string) => {
+  return {
+    eventAbbr: eventAbbr,
+    roomId: roomId,
+    roomType: roomType,
+    body: body,
+    messageType: ChatMessageMessageTypeEnum.Chat,
+  }
 }
 
 const ChatMessageForm: React.FC<Props> = ({ roomId }) => {
-    const api = new ChatMessageApi()
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { isSubmitSuccessful },
-    } = useForm<Inputs>()
-    const [submittedData, setSubmittedData] = useState({})
+  const api = new ChatMessageApi()
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful },
+  } = useForm<Inputs>()
+  const [submittedData, setSubmittedData] = useState({})
 
-    const onSubmit = (data: Inputs) => {
-        if (!roomId) return
-        console.log(data)
-        setSubmittedData(data)
-        const msg = {
-            eventAbbr: 'cndo2021',
-            roomId: roomId,
-            roomType: 'talk',
-            body: data.chatMessage,
-            messageType: ChatMessageMessageTypeEnum.Chat,
-        }
-        if (data.isQuestion) {
-            msg.messageType = ChatMessageMessageTypeEnum.Qa
-        }
-        api.apiV1ChatMessagesPost(msg)
+  const onSubmit = (data: Inputs) => {
+    if (!roomId) return
+    setSubmittedData(data)
+    const msg = ChatMessageRequest('cndo2021', roomId, 'talk', data.chatMessage)
+    if (data.isQuestion) {
+      msg.messageType = ChatMessageMessageTypeEnum.Qa
     }
-    useEffect(() => {
-        if (isSubmitSuccessful) {
-            reset({ chatMessage: '' })
-        }
-    }, [isSubmitSuccessful, submittedData, reset])
+    api.apiV1ChatMessagesPost(msg)
+  }
 
-    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-          <textarea name="chatMessage" ref={register} />
-          <input type="submit" /><br/>
-          <input type="checkbox" name="isQuestion" ref={register}/>質問を送る
-      </form>
-    )
+  const onThumbsUp = () => {
+    if (!roomId) return
+    const msg = ChatMessageRequest('cndo2021', roomId, 'talk', "👍")
+    api.apiV1ChatMessagesPost(msg)
+  }
+  const onClap = () => {
+    if (!roomId) return
+    const msg = ChatMessageRequest('cndo2021', roomId, 'talk', "👏")
+    api.apiV1ChatMessagesPost(msg)
+  }
+  const onPartyPopper = () => {
+    if (!roomId) return
+    const msg = ChatMessageRequest('cndo2021', roomId, 'talk', "🎉")
+    api.apiV1ChatMessagesPost(msg)
+  }
+
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      reset({ chatMessage: '' })
+    }
+  }, [isSubmitSuccessful, submittedData, reset])
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <textarea name="chatMessage" ref={register} />
+      <input type="submit" /><br/>
+      <input type="checkbox" name="isQuestion" ref={register}/>質問を送る<br/>
+      <Button onClick={onThumbsUp} variant="contained">👍</Button>
+      <Button onClick={onClap} variant="contained">👏</Button>
+      <Button onClick={onPartyPopper} variant="contained">🎉</Button>
+    </form>
+  )
 }
 
 export default ChatMessageForm
