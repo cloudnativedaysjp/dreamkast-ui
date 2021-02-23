94a2620773067028be0445c581a4d438478a39e5
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-23 11:23:51 +0900

    refine css

--- src/components/Chat/styled.ts
+++ src/components/Chat/styled.ts
@@ -1,26 +1,27 @@
-import styled from 'styled-components'
-import { Paper, Box as MaterialUIBox } from '@material-ui/core'
+import styled from "styled-components";
+import { Paper, Box as MaterialUIBox } from "@material-ui/core";
 
 export const Box = styled(MaterialUIBox)`
   height: 400px;
-`
+`;
 
 export const ChatMessage = styled(Paper)<{ isChat: boolean }>`
   padding: 10px;
-  background: ${({ isChat }) => (isChat ? '#ffffff' : '#f5f5f5')};
-  white-sace: pre-wrap;
-`
+  margin: 5px;
+  background: ${({ isChat }) => (isChat ? "#ffffff" : "#f5f5f5")};
+  white-space: pre-wrap;
+`;
 export const ChatReplyMessage = styled(ChatMessage)`
   margin-left: 20px;
-`
+`;
 
 export const ChatSelectedMessage = styled(ChatMessage)`
   background-color: cornsilk;
-`
+`;
 
 export const CloseReplyButton = styled.div`
   cursor: pointer;
   float: right;
   margintop: 5px;
   width: 20px;
-`
+`;
