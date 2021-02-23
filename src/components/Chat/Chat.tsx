2507e4c076eea5260d3c76d26727cfcdb6266f31
r_takaishi <ryo.takaishi.0@gmail.com>
2021-02-23 13:57:14 +0900

    state関係は近くに置く

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,86 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import { Tab, Tabs } from '@material-ui/core'
+import React, { useEffect, useState } from "react";
+import { Tab, Tabs } from "@material-ui/core";
 import {
   ChatMessageApi,
   ChatMessageMessageTypeEnum,
   Talk,
   Configuration,
-} from '../../client-axios'
-import ChatMessageForm from './ChatMessageForm'
-import ActionCable from 'actioncable'
-import { ChatMessageClass, ChatMessageMap } from './index'
-import { TabContext, TabPanel } from '@material-ui/lab'
-import { ChatBox } from './ChatBox'
+} from "../../client-axios";
+import ChatMessageForm from "./ChatMessageForm";
+import ActionCable from "actioncable";
+import { ChatMessageClass, ChatMessageMap } from "./index";
+import { TabContext, TabPanel } from "@material-ui/lab";
+import { ChatBox } from "./ChatBox";
 
 type Props = {
-  talk?: Talk
-}
+  talk?: Talk;
+};
 
 type ReceivedMsg = {
-  id: number
-  profileId: number
-  speakerId: number
-  eventAbbr: string
-  roomId: number
-  roomType: string
-  body: string
-  messageType: ChatMessageMessageTypeEnum
-  replyTo: number
-}
+  id: number;
+  profileId: number;
+  speakerId: number;
+  eventAbbr: string;
+  roomId: number;
+  roomType: string;
+  body: string;
+  messageType: ChatMessageMessageTypeEnum;
+  replyTo: number;
+};
 
 export const Chat: React.FC<Props> = ({ talk }) => {
-  const initialChatMessageMap = new ChatMessageMap()
+  const initialChatMessageMap = new ChatMessageMap();
   const initialChatMessage = {
-    eventAbbr: 'cndo2021',
-    body: '',
+    eventAbbr: "cndo2021",
+    body: "",
     messageType: ChatMessageMessageTypeEnum.Chat,
-  }
+  };
+  const [selectedTab, setSelectedTab] = useState("0");
   const [messages, setMessages] = useState<ChatMessageMap>(
-    initialChatMessageMap,
-  )
+    initialChatMessageMap
+  );
   const [selectedMessage, setSelectedMessage] = useState<ChatMessageClass>(
-    initialChatMessage,
-  )
+    initialChatMessage
+  );
   const actionCableUrl = () => {
-    if (window.location.protocol == 'http:') {
-      return `ws://${window.location.host}/cable`
+    if (window.location.protocol == "http:") {
+      return `ws://${window.location.host}/cable`;
     } else {
-      return `wss://${window.location.host}/cable`
+      return `wss://${window.location.host}/cable`;
     }
-  }
+  };
   const fetchChatMessagesFromAPI = (api: ChatMessageApi) => {
-    if (!talk || !messages) return
+    if (!talk || !messages) return;
     api
-      .apiV1ChatMessagesGet('cndo2021', String(talk.id), 'talk')
+      .apiV1ChatMessagesGet("cndo2021", String(talk.id), "talk")
       .then((res) => {
-        if (!messages) setMessages(new ChatMessageMap())
+        if (!messages) setMessages(new ChatMessageMap());
         res.data.forEach((receivedMsg) => {
-          messages.addMessage(receivedMsg)
-        })
-        setMessages(new ChatMessageMap(messages))
-      })
-  }
+          messages.addMessage(receivedMsg);
+        });
+        setMessages(new ChatMessageMap(messages));
+      });
+  };
 
   useEffect(() => {
     const api = new ChatMessageApi(
-      new Configuration({ basePath: window.location.origin }),
-    )
+      new Configuration({ basePath: window.location.origin })
+    );
 
-    if (!talk) return
+    if (!talk) return;
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const actionCable = require('actioncable')
+    const actionCable = require("actioncable");
 
-    fetchChatMessagesFromAPI(api)
-    const wsUrl = actionCableUrl()
-    const cableApp: ActionCable.Cable = actionCable.createConsumer(wsUrl)
+    fetchChatMessagesFromAPI(api);
+    const wsUrl = actionCableUrl();
+    const cableApp: ActionCable.Cable = actionCable.createConsumer(wsUrl);
     if (cableApp) {
-      cableApp.disconnect()
+      cableApp.disconnect();
     }
     cableApp.subscriptions.create(
-      { channel: 'ChatChannel', roomType: 'talk', roomId: talk.id },
+      { channel: "ChatChannel", roomType: "talk", roomId: talk.id },
       {
         received(receivedMsg: ReceivedMsg) {
-          if (!messages) return
+          if (!messages) return;
           const msg = new ChatMessageClass(
             receivedMsg.id,
             receivedMsg.profileId,
@@ -90,39 +91,40 @@ export const Chat: React.FC<Props> = ({ talk }) => {
             receivedMsg.roomType,
             receivedMsg.body,
             receivedMsg.messageType,
-            receivedMsg.replyTo,
-          )
-          messages.addMessage(msg)
-          setMessages(new ChatMessageMap(messages))
+            receivedMsg.replyTo
+          );
+          messages.addMessage(msg);
+          setMessages(new ChatMessageMap(messages));
         },
-      },
-    )
-  }, [talk])
-
-  const [selectedTab, setSelectedTab] = useState('0')
+      }
+    );
+  }, [talk]);
 
   function a11yProps(index: number) {
     return {
       id: `simple-tab-${index}`,
-      'aria-controls': `simple-tabpanel-${index}`,
-    }
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
   }
-  const handleChange = (_event: React.ChangeEvent<{}>, newValue: string) => {
-    setSelectedTab(newValue)
-  }
+  const handleChange = (
+    _event: React.ChangeEvent<Record<string, never>>,
+    newValue: string
+  ) => {
+    setSelectedTab(newValue);
+  };
 
   const onClickMessage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (!messages || Object.keys(messages).length == 0) return
-    const selectedMessageId = e.currentTarget.getAttribute('data-messageId')
-    if (!selectedMessageId) return
-    const msg = messages.get(Number(selectedMessageId))
-    if (!msg) return
-    setSelectedMessage(msg)
-  }
+    if (!messages || Object.keys(messages).length == 0) return;
+    const selectedMessageId = e.currentTarget.getAttribute("data-messageId");
+    if (!selectedMessageId) return;
+    const msg = messages.get(Number(selectedMessageId));
+    if (!msg) return;
+    setSelectedMessage(msg);
+  };
 
   const onClickCloseButton = () => {
-    setSelectedMessage(initialChatMessage)
-  }
+    setSelectedMessage(initialChatMessage);
+  };
 
   return (
     <div>
@@ -168,5 +170,5 @@ export const Chat: React.FC<Props> = ({ talk }) => {
         </TabPanel>
       </TabContext>
     </div>
-  )
-}
+  );
+};
