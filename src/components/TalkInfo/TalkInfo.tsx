e512e4d019587f2e4f77acfe50d97831cabc26e8
Kazuto Kusama <jacopen@gmail.com>
2021-03-04 17:05:20 +0900

    test

--- src/components/TalkInfo/TalkInfo.tsx
+++ src/components/TalkInfo/TalkInfo.tsx
@@ -3,11 +3,14 @@ import * as Styled from './styled'
 import { Talk } from '../../client-axios'
 
 type Props = {
-  selectedTalk?: Talk,
+  selectedTalk?: Talk
   selectedTrackName?: string
 }
 
-export const TalkInfo: React.FC<Props> = ({ selectedTalk, selectedTrackName }) => {
+export const TalkInfo: React.FC<Props> = ({
+  selectedTalk,
+  selectedTrackName,
+}) => {
   const twitterURL = (trackName?: string) => {
     const base =
       'http://twitter.com/share?url=https://event.cloudnativedays.jp/cndo2021&related=@cloudnativedays&hashtags=CNDO2021'
@@ -17,23 +20,30 @@ export const TalkInfo: React.FC<Props> = ({ selectedTalk, selectedTrackName }) =
 
   return (
     <Styled.OuterContainer>
-    <Styled.Container>
-      <Styled.Title>{selectedTalk?.title}</Styled.Title>
-      <h3>
-        {selectedTalk?.speakers
-          .map((speaker) => {
-            return speaker.name
-          })
-          .join(' / ')}
-      </h3>
-      <Styled.Content>{selectedTalk?.abstract}</Styled.Content>
-      <Styled.ShareButton href="https://discord.gg/bvuTMNQ" target="_blank">
-        <Styled.SocialImg src="/cndo2021/ui/images/discord.png" /><Styled.SocialText>Ask the speaker</Styled.SocialText>
-      </Styled.ShareButton>
-      <Styled.ShareButton href={twitterURL(selectedTrackName)} target="_blank">
-        <Styled.SocialImg src="/cndo2021/ui/images/twitter.png" /><Styled.SocialText>Twitterでつぶやく #CNDT2020_{selectedTrackName}</Styled.SocialText>
-      </Styled.ShareButton>
-    </Styled.Container>
-  </Styled.OuterContainer>
+      <Styled.Container>
+        <Styled.Title>{selectedTalk?.title}</Styled.Title>
+        <h3>
+          {selectedTalk?.speakers
+            .map((speaker) => {
+              return speaker.name
+            })
+            .join(' / ')}
+        </h3>
+        <Styled.Content>{selectedTalk?.abstract}</Styled.Content>
+        <Styled.ShareButton href="https://discord.gg/bvuTMNQ" target="_blank">
+          <Styled.SocialImg src="/cndo2021/ui/images/discord.png" />
+          <Styled.SocialText>Ask the speaker-</Styled.SocialText>
+        </Styled.ShareButton>
+        <Styled.ShareButton
+          href={twitterURL(selectedTrackName)}
+          target="_blank"
+        >
+          <Styled.SocialImg src="/cndo2021/ui/images/twitter.png" />
+          <Styled.SocialText>
+            Twitterでつぶやく #CNDT2020_{selectedTrackName}
+          </Styled.SocialText>
+        </Styled.ShareButton>
+      </Styled.Container>
+    </Styled.OuterContainer>
   )
 }
