d8cc56a4e52dceee9f7a15778c918d57dc8f729a
inductor <kohei.ota@hpe.com>
2021-12-19 03:28:03 +0900

    Add Sentry

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   basePath: '/cndt2021/ui',
   assetPrefix: '/cndt2021/ui',
   publicRuntimeConfig: {
     basePath: '/cndt2021/ui',
   },
-}
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
