68ea6b993da11c263843a63caaad606c0a38b59f
Hiroki Okui <hiroki.okui@gmail.com>
2022-11-14 14:58:12 +0900

    feat: Add point when stamped

--- src/components/TrailMap/StampCard.tsx
+++ src/components/TrailMap/StampCard.tsx
@@ -2,7 +2,108 @@ import React, { useEffect, useState } from 'react'
 import * as Styled from './styled'
 import { useSelector } from 'react-redux'
 import { settingsSelector, stampSelector } from '../../store/settings'
-import { usePostApiV1AppDataByProfileIdConferenceAndConferenceMutation } from '../../generated/dreamkast-api.generated'
+import {
+  usePostApiV1AppDataByProfileIdConferenceAndConferenceMutation,
+  usePostApiV1ProfileByProfileIdPointMutation,
+} from '../../generated/dreamkast-api.generated'
+import { createHash } from 'crypto'
+
+type Props = {
+  todo?: boolean
+}
+
+export const StampCard = (_: Props) => {
+  const settings = useSelector(settingsSelector)
+  const stamp = useSelector(stampSelector)
+  const [pinnedStamp, setPinnedStamp] = useState<typeof stamp | null>(null)
+  const [stamped, setStamped] = useState<boolean>(false)
+  const [mutateAppData] =
+    usePostApiV1AppDataByProfileIdConferenceAndConferenceMutation()
+  const [postPointEvent] = usePostApiV1ProfileByProfileIdPointMutation()
+
+  useEffect(() => {
+    if (!stamp.initialized || pinnedStamp !== null) {
+      return
+    }
+    setPinnedStamp(stamp)
+  }, [stamp, pinnedStamp])
+
+  useEffect(() => {
+    if (!settings.initialized) {
+      return
+    }
+    if (!stamp.canGetNewStamp) {
+      return
+    }
+    ;(async () => {
+      if (!stamp.slotIdToBeStamped) {
+        return
+      }
+      const pointEventId = getPointEventId(
+        settings.eventAbbr,
+        stamp.slotIdToBeStamped,
+      )
+      try {
+        await postPointEvent({
+          profileId: `${settings.profile.id}`,
+          profilePoint: {
+            conference: settings.eventAbbr,
+            pointEventId,
+          },
+        })
+        await mutateAppData({
+          profileId: `${settings.profile.id}`,
+          conference: settings.eventAbbr,
+          dkUiDataMutation: {
+            action: 'stampedFromUI',
+            payload: {
+              slotId: stamp.slotIdToBeStamped,
+            },
+          },
+        })
+        setStamped(true)
+      } catch (err) {
+        console.error('stampFromUI Action', err)
+      }
+    })()
+  }, [settings.initialized, stamp.canGetNewStamp])
+
+  if (!pinnedStamp) {
+    return <></>
+  }
+
+  return (
+    <>
+      <Styled.Container>
+        <Styled.TrailMapImg src={`/cndt2022/ui/wave.jpg`}></Styled.TrailMapImg>
+        {stampLocation.map((loc, i) => (
+          <Styled.StampFrame top={`${loc.top}%`} left={`${loc.left}%`}>
+            {i < pinnedStamp.stamps.length ? (
+              <Styled.Stamp
+                src={`/cndt2022/ui/cndt2022_trademark.png`}
+              ></Styled.Stamp>
+            ) : (
+              ''
+            )}
+            {i === pinnedStamp.stamps.length && stamped ? (
+              <Styled.Stamp
+                className={'showAnimation'}
+                src={`/cndt2022/ui/cndt2022_trademark.png`}
+              ></Styled.Stamp>
+            ) : (
+              ''
+            )}
+          </Styled.StampFrame>
+        ))}
+      </Styled.Container>
+    </>
+  )
+}
+
+function getPointEventId(confName: string, slotId: number): string {
+  const shasum = createHash('sha1')
+  return shasum.update(`${confName}/10${slotId}`).digest('hex')
+}
 
 const stampLocation = [
   {
@@ -55,80 +156,3 @@ const stampLocation = [
     left: 80,
   },
 ]
-
-type Props = {
-  todo?: boolean
-}
-
-export const StampCard = (_: Props) => {
-  const settings = useSelector(settingsSelector)
-  const stamp = useSelector(stampSelector)
-  const [pinnedStamp, setPinnedStamp] = useState<typeof stamp | null>(null)
-  const [stamped, setStamped] = useState<boolean>(false)
-  const [mutateAppData] =
-    usePostApiV1AppDataByProfileIdConferenceAndConferenceMutation()
-
-  useEffect(() => {
-    if (!stamp.initialized || pinnedStamp !== null) {
-      return
-    }
-    setPinnedStamp(stamp)
-  }, [stamp, pinnedStamp])
-
-  useEffect(() => {
-    if (!settings.initialized) {
-      return
-    }
-    if (stamp.canGetNewStamp) {
-      // TODO set point
-      mutateAppData({
-        profileId: `${settings.profile.id}`,
-        conference: settings.eventAbbr,
-        dkUiDataMutation: {
-          action: 'stampedFromUI',
-          payload: {
-            slotId: stamp.slotIdToBeStamped,
-          },
-        },
-      })
-        .unwrap()
-        .then(() => {
-          setStamped(true)
-        })
-        .catch(() => {
-          console.error('stampedFromUI action')
-        })
-    }
-  }, [settings.initialized, stamp.canGetNewStamp])
-
-  if (!pinnedStamp) {
-    return <></>
-  }
-
-  return (
-    <>
-      <Styled.Container>
-        <Styled.TrailMapImg src={`/cndt2022/ui/wave.jpg`}></Styled.TrailMapImg>
-        {stampLocation.map((loc, i) => (
-          <Styled.StampFrame top={`${loc.top}%`} left={`${loc.left}%`}>
-            {i < pinnedStamp.stamps.length ? (
-              <Styled.Stamp
-                src={`/cndt2022/ui/cndt2022_trademark.png`}
-              ></Styled.Stamp>
-            ) : (
-              ''
-            )}
-            {i === pinnedStamp.stamps.length && stamped ? (
-              <Styled.Stamp
-                className={'showAnimation'}
-                src={`/cndt2022/ui/cndt2022_trademark.png`}
-              ></Styled.Stamp>
-            ) : (
-              ''
-            )}
-          </Styled.StampFrame>
-        ))}
-      </Styled.Container>
-    </>
-  )
-}
