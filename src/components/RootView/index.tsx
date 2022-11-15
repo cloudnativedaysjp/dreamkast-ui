e9f8939c752ad058d55e43047ddd61ea5f92dada
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2022-11-15 19:16:57 +0900

    unnecessary component

--- src/components/RootView/index.tsx
+++
@@ -1,1 +0,0 @@
-export { RootView } from './RootView'
--- src/pages/[eventAbbr]/ui/index.tsx
+++ src/pages/[eventAbbr]/ui/index.tsx
@@ -1,5 +1,8 @@
 import { useState, useEffect, useMemo, useCallback } from 'react'
 import { useRouter } from 'next/router'
+import { Layout } from '../../../components/Layout'
+import { TrackSelector } from '../../../components/TrackSelector'
+import { TrackView } from '../../../components/Track'
 import { isStorageAvailable } from '../../../util/sessionstorage'
 import {
   useGetApiV1TracksQuery,
@@ -10,7 +13,6 @@ import {
 import { NextPage } from 'next'
 import { setProfile } from '../../../store/settings'
 import { useDispatch } from 'react-redux'
-import { RootView } from '../../../components/RootView/RootView'
 
 const IndexPage: NextPage = () => {
   const router = useRouter()
@@ -67,12 +69,14 @@ const IndexPage: NextPage = () => {
 
   if (event) {
     return (
-      <RootView
-        event={event}
-        tracks={v1TracksQuery.data ?? []}
-        selectedTrack={selectedTrack}
-        selectTrack={selectTrack}
-      />
+      <Layout title={event.name} event={event}>
+        <TrackSelector
+          tracks={v1TracksQuery.data ?? []}
+          selectedTrack={selectedTrack}
+          selectTrack={selectTrack}
+        />
+        <TrackView event={event} selectedTrack={selectedTrack} />
+      </Layout>
     )
   } else {
     return <div></div>
