a384560991855e5d3353d4a25a04a28e96b4913e
Hiroki Okui <hiroki.okui@gmail.com>
2023-02-18 17:47:40 +0900

    refactor: run prettier

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,8 @@ export const ENV = {
   NEXT_PUBLIC_AUTH0_AUDIENCE: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE ?? '',
   NEXT_PUBLIC_BASE_PATH: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
   NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL ?? '',
-  NEXT_PUBLIC_EVENT_SALT: process.env.NEXT_PUBLIC_EVENT_SALT ?? 'cloudnativedays',
+  NEXT_PUBLIC_EVENT_SALT:
+    process.env.NEXT_PUBLIC_EVENT_SALT ?? 'cloudnativedays',
 } as const
 
 export function validateEnv() {
