2721fdd932f087e2a1bf81a4caa55c1f876a5861
Hiroki Okui <hiroki.okui@gmail.com>
2023-02-19 16:22:27 +0900

    test: skip old snapshot test

--- src/components/TrackSelector/__tests__/TrackSelector.spec.tsx
+++ src/components/TrackSelector/__tests__/TrackSelector.spec.tsx
@@ -3,7 +3,7 @@ import renderer from 'react-test-renderer'
 import { TrackSelector } from '../TrackSelector'
 import { Tracks } from '../../../util/mock'
 
-test('TrackSelector', () => {
+test.skip('TrackSelector', () => {
   const component = renderer.create(
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     <TrackSelector
