e9a71b11f4715321a8c03683fff1a4f9fd4a19e4
Hiroki Okui <hiroki.okui@gmail.com>
2023-02-19 11:41:48 +0900

    feat: load all talks at top-level page component

--- src/pages/[eventAbbr]/ui/index.tsx
+++ src/pages/[eventAbbr]/ui/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
 import { Layout } from '../../../components/Layout'
 import { TrackSelector } from '../../../components/TrackSelector'
 import { TrackView } from '../../../components/Track'
@@ -6,19 +6,59 @@ import { isStorageAvailable } from '../../../util/sessionstorage'
 import {
   useGetApiV1TracksQuery,
   Track,
+  useGetApiV1TalksQuery,
 } from '../../../generated/dreamkast-api.generated'
 import { NextPage } from 'next'
 import { useInitSetup } from '../../../components/hooks/useInitSetup'
 import { useAppDataSetup } from '../../../components/hooks/useAppDataSetup'
+import dayjs from 'dayjs'
+import { useDispatch } from 'react-redux'
+import { setTalks } from '../../../store/settings'
 
 const IndexPage: NextPage = () => {
+  const dispatch = useDispatch()
   const { eventAbbr, event } = useInitSetup()
+  const [_, setError] = useState()
   useAppDataSetup()
 
   const v1TracksQuery = useGetApiV1TracksQuery(
     { eventAbbr },
     { skip: !eventAbbr },
   )
+
+  const dayId = useMemo(() => {
+    const today = dayjs(new Date()).tz('Asia/Tokyo').format('YYYY-MM-DD')
+    let dayId = ''
+    event?.conferenceDays?.forEach((day) => {
+      if (day.date == today && day.id) {
+        dayId = String(day.id)
+      }
+    })
+    return dayId
+  }, [event])
+
+  const { data, isLoading, isError, error } = useGetApiV1TalksQuery(
+    {
+      eventAbbr: eventAbbr,
+      conferenceDayIds: dayId,
+    },
+    { skip: !dayId },
+  )
+
+  useEffect(() => {
+    if (isLoading) {
+      return
+    }
+    if (isError) {
+      setError(() => {
+        throw error
+      })
+      return
+    }
+    if (data) {
+      dispatch(setTalks(data))
+    }
+  }, [data, isLoading, isError])
 
   const getTrack = () => {
     if (!v1TracksQuery.data) {
