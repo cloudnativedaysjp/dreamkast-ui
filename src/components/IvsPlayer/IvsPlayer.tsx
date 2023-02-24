4e70a2a30a3245963fd7c5dff16856a8a5a1992a
masafumi yamamoto <ureuzy@gmail.com>
2023-02-25 02:53:34 +0900

    fix: unnecessary imports

--- src/components/IvsPlayer/IvsPlayer.tsx
+++ src/components/IvsPlayer/IvsPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import videojs, { VideoJsPlayer } from 'video.js'
 import 'video.js/dist/video-js.css'
 
