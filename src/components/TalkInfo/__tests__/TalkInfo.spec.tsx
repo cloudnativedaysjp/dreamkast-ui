93b7b00e5aee0694ecc32ef5a09c6d9ec9b1b904
Hiroki Okui <hiroki.okui@gmail.com>
2023-02-24 22:03:27 +0900

    test: change test fixture to the same one with backend db seeds

--- src/components/TalkInfo/__tests__/TalkInfo.spec.tsx
+++ src/components/TalkInfo/__tests__/TalkInfo.spec.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
-import { MockEvent, Talks } from '../../../util/mock'
+import { MockEvent, MockTalks } from '../../../util/mock'
 import { TalkInfo } from '../TalkInfo'
 
 // TODO fix following by extracting Presentation Component
 test.skip('TalkInfo', () => {
   const component = renderer.create(
-    <TalkInfo event={MockEvent} selectedTalk={Talks[0]} />,
+    <TalkInfo event={MockEvent} selectedTalk={MockTalks[0]} />,
   )
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
--- src/components/TrackSelector/__tests__/TrackSelector.spec.tsx
+++ src/components/TrackSelector/__tests__/TrackSelector.spec.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import { TrackSelector } from '../TrackSelector'
-import { Tracks } from '../../../util/mock'
+import { MockTracks } from '../../../util/mock'
 
 test.skip('TrackSelector', () => {
   const component = renderer.create(
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     <TrackSelector
-      selectedTrack={Tracks[0]}
+      selectedTrack={MockTracks[0]}
       // eslint-disable-next-line @typescript-eslint/no-empty-function
       selectTrack={() => {}}
     />,
