45c63da5a59769c3126d689b7287044e18ed7186
Hiroki Okui <hiroki.okui@gmail.com>
2023-03-02 00:01:58 +0900

    feat: Add error view

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -18,13 +18,14 @@
 
 import * as Sentry from '@sentry/nextjs'
 import NextErrorComponent from 'next/error'
+import { ErrorLayout } from '../components/Layout/ErrorLayout'
 
 const CustomErrorComponent = (props) => {
   // If you're using a Nextjs version prior to 12.2.1, uncomment this to
   // compensate for https://github.com/vercel/next.js/issues/8592
   // Sentry.captureUnderscoreErrorException(props)
 
-  return <NextErrorComponent statusCode={props.statusCode} />
+  return <ErrorLayout statusCode={props.statusCode} />
 }
 
 CustomErrorComponent.getInitialProps = async (contextData) => {
