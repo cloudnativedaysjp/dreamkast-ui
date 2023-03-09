f46af63a2a533917d46e7e24e9e8a97af4f78212
Hiroki Okui <hiroki.okui@gmail.com>
2023-03-09 16:44:12 +0900

    fix: Adjust history card view

--- src/components/TrailMap/internal/TrailMapPointHistory.tsx
+++ src/components/TrailMap/internal/TrailMapPointHistory.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import * as Styled from '../styled'
 import {
-  Paper,
   Table,
   TableBody,
   TableCell,
@@ -10,30 +9,42 @@ import {
   TableRow,
 } from '@material-ui/core'
 import { ProfilePointsResponse } from '../../../generated/dreamkast-api.generated'
+import { createStyles, withStyles } from '@material-ui/styles'
 
 type Props = {
   pointData: ProfilePointsResponse
 }
 
+const StyledTableCell = withStyles(() =>
+  createStyles({
+    head: {
+      backgroundColor: '#efebe9',
+    },
+    body: {
+      backgroundColor: '#efebe9',
+    },
+  }),
+)(TableCell)
+
 export const TrailMapPointHistory = ({ pointData }: Props) => {
   return (
     <Styled.PointHistoryTable>
-      <TableContainer component={Paper}>
+      <TableContainer>
         <Table aria-label="event-history" size="small">
           <TableHead>
             <TableRow>
-              <TableCell>獲得ポイント</TableCell>
-              <TableCell>達成条件</TableCell>
+              <StyledTableCell>獲得ポイント</StyledTableCell>
+              <StyledTableCell>達成条件</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {pointData.points.map((p) => {
               return (
                 <TableRow key={p.pointEventId}>
-                  <TableCell component="th" scope="row">
+                  <StyledTableCell component="th" scope="row">
                     {p.point}pt
-                  </TableCell>
-                  <TableCell>{p.desc}</TableCell>
+                  </StyledTableCell>
+                  <StyledTableCell>{p.desc}</StyledTableCell>
                 </TableRow>
               )
             })}
