d87f72cf142af72962de3fc94cc03d84b0ae0359
Hiroki Okui <hiroki.okui@gmail.com>
2023-03-10 17:58:26 +0900

    fix: update test snapshot

--- src/components/TrailMap/internal/__tests__/TrailMapPoint.spec.tsx
+++ src/components/TrailMap/internal/__tests__/TrailMapPoint.spec.tsx
@@ -3,12 +3,23 @@ import { render } from '@testing-library/react'
 import { TrailMapPoint } from '../TrailMapPoint'
 
 describe('TrailMapPoint', () => {
-  it('should render without crash', () => {
+  it('should render loading skeleton', () => {
     const data = {
       total: 20,
       points: [],
     }
     const screen = render(<TrailMapPoint pointData={data}></TrailMapPoint>)
+    expect(screen.asFragment()).toMatchSnapshot()
+  })
+
+  it('should render loading skeleton when loading pointData', () => {
+    const data = {
+      total: 0,
+      points: [],
+    }
+    const screen = render(
+      <TrailMapPoint isLoading={true} pointData={data}></TrailMapPoint>,
+    )
     expect(screen.asFragment()).toMatchSnapshot()
   })
 
