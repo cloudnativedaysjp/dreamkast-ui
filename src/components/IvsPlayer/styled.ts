6c2f3cc5f778485e9583dafba6462d3303bd6508
Hiroki Okui <hiroki.okui@gmail.com>
2023-03-18 21:14:16 +0900

    fix: make overlay component no-transparency

--- src/components/IvsPlayer/styled.ts
+++ src/components/IvsPlayer/styled.ts
@@ -17,7 +17,7 @@ export const IvsPlayerVideo = styled.video`
 `
 
 export const OverLayContainer = styled.div`
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(70, 70, 70);
   position: absolute;
   top: 0;
   left: 0;
