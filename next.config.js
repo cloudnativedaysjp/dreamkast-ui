dbb6f2c1e43bee9a02444944845e940050d52d34
inductor <kela@inductor.me>
2023-05-30 00:36:43 +0900

    add sentry replay

--- next.config.js
+++ next.config.js
@@ -6,10 +6,42 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withSentryConfig } = require('@sentry/nextjs')
 
-module.exports = {}
+// your existing module.exports or default export
+const nextConfig = {
+  // Optional build-time configuration options
+  sentry: {
+    hideSourceMaps: true,
+    // See the sections below for information on the following options:
+    //   'Configure Source Maps':
+    //     - disableServerWebpackPlugin
+    //     - disableClientWebpackPlugin
+    //     - hideSourceMaps
+    //     - widenClientFileUpload
+    //   'Configure Legacy Browser Support':
+    //     - transpileClientSDK
+    //   'Configure Serverside Auto-instrumentation':
+    //     - autoInstrumentServerFunctions
+    //     - excludeServerRoutes
+    //   'Configure Tunneling to avoid Ad-Blockers':
+    //     - tunnelRoute
+  },
+}
 
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true },
-  { hideSourceMaps: false }, // FIXME: 本来は望ましくないので、あとでソースマップはオフにする
-)
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  org: 'cloudnative-days',
+  project: 'dreamkast-ui',
+
+  silent: true, // Suppresses all logs
+
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+}
+
+// Make sure adding Sentry options is the last code to run before exporting
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
