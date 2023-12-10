5fde26ea99e35981d1fd89a6498e56b239ef169f
Hiroki Okui <hiroki.okui@gmail.com>
2023-12-10 20:15:18 +0900

    fix: Set max-height to slick-slider

--- src/components/Sponsors/styled.ts
+++ src/components/Sponsors/styled.ts
@@ -3,7 +3,9 @@ import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-export const CNDOSlider = styled(Slider)``
+export const CNDOSlider = styled(Slider)`
+  max-height: 54px;
+`
 
 export const Sponsor = styled.div`
   position: relative;
