43529c795878a56061c40ac39e5605a6f8150c89
Ryo Takaishi <ryo.takaishi.0@gmail.com>
2024-04-07 14:36:46 +0900

    read QR Code

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -1,8 +1,6 @@
-import React, {
-  useCallback,
-  useEffect,
-  useRef,
-} from 'react'
+'use client'
+import React, { useCallback, useEffect, useRef } from 'react'
+import jsQR from 'jsqr'
 
 type Props = {
   height: number
@@ -11,6 +9,8 @@ type Props = {
 
 export const Camera: React.FC<Props> = ({ height, width }) => {
   const videoRef = useRef<HTMLVideoElement>(null)
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+
   const getStream = useCallback(async () => {
     const aspectRatio = height / width
     return await navigator.mediaDevices.getUserMedia({
@@ -37,6 +37,7 @@ export const Camera: React.FC<Props> = ({ height, width }) => {
       }
       video.srcObject = stream
       video.play()
+      scanQrCode()
     }
 
     setVideo()
@@ -55,13 +56,45 @@ export const Camera: React.FC<Props> = ({ height, width }) => {
     return cleanupVideo
   }, [getStream])
 
+  const scanQrCode = () => {
+    const canvas = canvasRef.current
+    const video = videoRef.current
+    if (canvas && video) {
+      const ctx = canvas.getContext('2d')
+      if (ctx) {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+        const qrCodeData = jsQR(
+          imageData.data,
+          imageData.width,
+          imageData.height,
+        )
+        if (qrCodeData) {
+          const rawItem = localStorage.getItem('profiles')
+          const profiles = JSON.parse(rawItem || '[]')
+          const profilesSet = new Set(profiles)
+          profilesSet.add(qrCodeData.data)
+          console.log(Array.from(profilesSet))
+          localStorage.setItem('profiles', JSON.stringify(Array.from(profilesSet)))
+          setTimeout(scanQrCode, 100)
+        }
+        setTimeout(scanQrCode, 100)
+      }
+    }
+  }
+
   return (
-    <video
-      ref={videoRef}
-      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-      autoPlay
-      muted
-      playsInline
-    />
+    <div>
+      <video
+        ref={videoRef}
+        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        autoPlay
+        muted
+        playsInline
+      />
+      <div style={{ display: 'none' }}>
+        <canvas ref={canvasRef} id="js-canvas"></canvas>
+      </div>
+    </div>
   )
 }
