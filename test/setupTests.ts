307acd7279f831fd527e90386129dc55771b0080
さっちゃん <ne-sachirou@hatena.ne.jp>
2026-04-20 23:26:58 +0900

    ReferenceError: TextEncoder is not defined

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { TextDecoder, TextEncoder } from 'util'
+
+if (typeof globalThis.TextEncoder === 'undefined') {
+  globalThis.TextEncoder = TextEncoder
+}
+
+if (typeof globalThis.TextDecoder === 'undefined') {
+  globalThis.TextDecoder = TextDecoder as typeof globalThis.TextDecoder
+}
